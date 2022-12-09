a4d412692752d275264a7cbe38fa03cb51f68abd
pratim-vv <pratimvasireddy@gmail.com>
2022-12-09 17:37:09 -0600

    Added comments for readibility

--- export.js
+++ export.js
@@ -3,8 +3,8 @@ const spawner = require('child_process').spawn;
 const information = 'ur mom';
 console.log('Data sent to python script:', information);
   
-const python_process = spawner('python', ['./receive.py', information]);
+const python_process = spawner('python', ['./receive.py', information]); //sends information (a string) to python file "receive.py"
 
-python_process.stdout.on('data', (data) => {
+python_process.stdout.on('data', (data) => { //reads the python output
     console.log("finale:", data.toString());
 });
--- receive.py
+++ receive.py
@@ -3,6 +3,6 @@ import sys
 def test(check) :
     return check[0:2] + " test"
 
-input = sys.argv[1]
-print(test(input))
+input = sys.argv[1] #receives js string
+print(input.split('m')) #whatever you want to export back to the js file
 sys.stdout.flush()
