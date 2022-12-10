37fe43f2a6fb23d012239aac8604769d4eba0181
Taisia Kalinina <93149698+TaisiaK@users.noreply.github.com>
2022-12-10 16:33:40 -0600

    Update myscripts.js
    added functions to get information from the backend and reform it

--- myscripts.js
+++ myscripts.js
@@ -12,10 +12,21 @@ function look_up() {
     }
 }
 
-//user imput
-const classInfo = "";
+//processing  data from back end
+function toArr(data) {
+    const twoD = data.split("@");
+    for (let arr of twoD) {
+        arr = arr.split("+");
+        for (let elem of arr) {
+            elem = elem.split(",");
+        }
+    }
+    return twoD;
+}
+
 let btn = document.getElementById('btn');
 btn.addEventListener('click', event => {
+    //getting user imput
     var monday = document.getElementById('m').value;
     var tuesday = document.getElementById('t').value;
     var wednesday = document.getElementById('w').value;
@@ -24,33 +35,28 @@ btn.addEventListener('click', event => {
     var dept = document.getElementById('searchbar').value;
     const user_input = dept + "+" + monday + "+" + tuesday + "+" + wednesday + "+" + thursday + "+" + friday;
     console.log(JSON.stringify(user_input));
+    //sending information to the backend
     const information = JSON.stringify(user_input);
     const spawner = require('child_process').spawn;
-    const python_process = spawner('python', ['./test.py', information]); //sends information to backend   
+    const python_process = spawner('python', ['./better_schedule.py', information]); 
+    //gettting information from the backend 
+    const classInfo = "";
     python_process.stdout.on('data', (data) => { //reads the python output
        classInfo = data.toString();
-    });    
-});
-
-//processing  data from back end
-var classTable = [
-    [[['Intro Computing: Engrg & Sci 101'], ['Lecture', "one"], ['Laboratory-Discussion', "two"]],
-    [['Little Bits to Big Ideas 102'], ['NO VIABLE SECTIONS']], 
-    [['Intro Computing: Non-Tech 105'], ['Lecture', "one"], ['Laboratory-Discussion', "one", "two", "three", "four", "five"]]]
-]
-
-//displaying the output from the back end
-const out = document.getElementById("output");
-let table = document.createElement('table');
-for (let one of classTable) {
-    for (let two of one) {
-        for (let row of two) {
-            table.insertRow();
-            for (let cell of row) {
-                let newCell = table.rows[table.rows.length - 1].insertCell();
-                newCell.textContent = cell;
+    }); 
+    var classTable = toArr(classInfo); 
+    const out = document.getElementById("output");
+    let table = document.createElement('table');
+    for (let one of classTable) {
+        for (let two of one) {
+            for (let row of two) {
+                table.insertRow();
+                for (let cell of row) {
+                    let newCell = table.rows[table.rows.length - 1].insertCell();
+                    newCell.textContent = cell;
+                }
             }
         }
     }
-}
-out.appendChild(table);
+    out.appendChild(table);
+});
